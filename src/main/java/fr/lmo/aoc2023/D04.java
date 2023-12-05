6103719326004d4f37d08d7843ad328f96c9f751
leo.mouly <leo.mouly@royalcanin.com>
2023-12-05 10:58:56 +0100

    2023 D04: Part 1

--- src/main/java/fr/lmo/aoc2023/D04.java
+++ src/main/java/fr/lmo/aoc2023/D04.java
@@ -1,28 +1,55 @@
 package fr.lmo.aoc2023;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
-import java.util.function.Predicate;
-import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import static java.util.function.Predicate.not;
 
 
 public class D04 extends AoCHelper {
 
     public void run() {
+
         // 1st part
-        System.out.println("Test : " + part1(stream(getTestInputPath())));
-        System.out.println("Real file : " + part1(stream(getInputPath())));
+        System.out.println("Test : " + findWinningPoints(stream(getTestInputPath())));
+        System.out.println("Real file : " + findWinningPoints(stream(getInputPath())));
 
-        // 2d part
-        System.out.println("Test : " + part1(stream(getTestInputPath())));
-        System.out.println("Real file : " + part1(stream(getInputPath())));
+        // 2d part TODO
+        // System.out.println("Test : " + findWinningPoints(stream(getTestInputPath())));
+        //    System.out.println("Real file : " + findWinningPoints(stream(getInputPath())));
     }
 
-    private String part1(Stream<String> stream) {
-        return "";
+    private int findWinningPoints(Stream<String> stream) {
+        return  stream.map(Card::new)
+                .mapToInt(Card::computePoints)
+                .sum();
     }
 
+    record Card(int id, Set<Integer> winning, List<Integer> draw) {
+        // Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        public Card(String cardLine) {
+            this(
+                    Integer.parseInt(cardLine.substring(5, cardLine.indexOf(":")).trim()),
+                    Arrays.stream(cardLine.substring(cardLine.indexOf(":") + 1).split("\\|")[0].trim().split(" "))
+                            .map(String::trim)
+                            .filter(not(String::isBlank))
+                            .mapToInt(Integer::parseInt).boxed().collect(Collectors.toSet()),
+                    Arrays.stream(cardLine.split("\\|")[1].trim().split(" "))
+                            .map(String::trim)
+                            .filter(not(String::isBlank))
+                            .mapToInt(Integer::parseInt).boxed()
+                            .toList()
+            );
+        }
 
+        public int computePoints() {
+            long count = draw.stream()
+                    .filter(winning::contains) // je garde juste les gagnants
+                    .count();
+            return (int) Math.pow(2, count - 1);
+        }
+    }
 }
