388893c7f569c2904ffe28abb1a5f1fa3a1e73bc
leo.mouly <leo.mouly@royalcanin.com>
2023-12-31 14:14:04 +0100

    2023 D07: Setup

---
+++ src/main/java/fr/lmo/aoc2023/D07.java
@@ -0,0 +1,38 @@
+package fr.lmo.aoc2023;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.LongPredicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+
+public class D07 extends AoCHelper {
+
+    public void run() {
+        System.out.println("Test : " + findWinningWays(parseHands(getTestInputPath())));
+        System.out.println("Real file : " + findWinningWays(parseHands(getInputPath())));
+    }
+
+    private long findWinningWays(List<Hand> hands) {
+        return 1;
+    }
+
+    record Hand() {}
+
+
+    public static void main(String[] args) {
+        AoCHelper.main(args);
+    }
+
+    /**
+     * CopyPasted from @elacazed, parsing input strings is not my cup of tea (and isn't the purpose of AoC...  I guess :p)
+     */
+    List<Hand> parseHands(Path path) {
+        List<String> lines = list(path);
+        String[] times = lines.get(0).substring("Time:".length()).trim().split("\s+");
+        String[] distances = lines.get(1).substring("Distance:".length()).trim().split("\s+");
+        return IntStream.range(0, times.length).mapToObj(i -> new Hand().toList();
+    }
+}
